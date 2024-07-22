e7fe99c6bf469871c97be9e983f1ce358a9bd9ae
sebqa <sebqa3009@gmail.com>
2024-07-22 21:52:19 +0200

    icons

--- lib/recommended_documents_lv.dart
+++ lib/recommended_documents_lv.dart
@@ -9,19 +9,12 @@ class RecommendedDocumentsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (recommendedDocuments == null) {
-      throw ArgumentError.notNull('recommendedDocuments');
-    }
     return SizedBox(
       height: 150, // Set the height of the horizontal ListView
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: recommendedDocuments.length,
         itemBuilder: (context, index) {
-          if (recommendedDocuments[index] == null) {
-            throw ArgumentError.notNull('recommendedDocuments[$index]');
-          }
-          // Build your recommended Document widget here
           return RecommendedDocumentWidget(recommendedDocuments[index]);
         },
       ),
