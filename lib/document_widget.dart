744d41df60134c8e196658d1e81392d0b3c4e229
sebqa <sebqa3009@gmail.com>
2024-07-26 18:31:32 +0200

    icon væk

--- lib/document_widget.dart
+++ lib/document_widget.dart
@@ -51,13 +51,13 @@ Color(0xff283593),
         : document.symbolCode == 'cloudy'
             ? CloudWidget(
                 cloudConfig: CloudConfig(
-                  size: 100, color: Color(0xaaffffff), icon: IconData(63056, fontFamily: 'MaterialIcons'), widgetCloud: null, x: 70, y: 5, scaleBegin: 1, scaleEnd: 1.1, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 11, slideY: 5, slideDurMill: 2000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),
+                  size: 100, color: Color(0xaaffffff), widgetCloud: null, x: 70, y: 5, scaleBegin: 1, scaleEnd: 1.1, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 11, slideY: 5, slideDurMill: 2000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),
                 
               )
               : document.symbolCode == 'partlycloudy_day'
             ? CloudWidget(
                 cloudConfig: CloudConfig(
-                  size: 50, color: Color(0xaaffffff), icon: IconData(63056, fontFamily: 'MaterialIcons'), widgetCloud: null, x: 120, y: 5, scaleBegin: 1, scaleEnd: 1.1, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 11, slideY: 5, slideDurMill: 2000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),
+                  size: 50, color: Color(0xaaffffff), widgetCloud: null, x: 120, y: 5, scaleBegin: 1, scaleEnd: 1.1, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 11, slideY: 5, slideDurMill: 2000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),
                 
               )
           :document.symbolCode == 'rainy'
@@ -70,9 +70,9 @@ Color(0xffcfd8dc),
 ],
     children: [
       RainWidget(rainConfig: RainConfig(count: 30, lengthDrop: 13, widthDrop: 4, color: Color(0xff9e9e9e), isRoundedEndsDrop: true, widgetRainDrop: null, fallRangeMinDurMill: 500, fallRangeMaxDurMill: 1500, areaXStart: 41, areaXEnd: 264, areaYStart: 208, areaYEnd: 620, slideX: 2, slideY: 0, slideDurMill: 2000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00), fallCurve: Cubic(0.55, 0.09, 0.68, 0.53), fadeCurve: Cubic(0.95, 0.05, 0.80, 0.04)),),
-CloudWidget(cloudConfig: CloudConfig(size: 270, color: Color(0xcdbdbdbd), icon: IconData(63056, fontFamily: 'MaterialIcons'), widgetCloud: null, x: 119, y: -50, scaleBegin: 1, scaleEnd: 1.1, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 11, slideY: 13, slideDurMill: 4000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),),
-CloudWidget(cloudConfig: CloudConfig(size: 250, color: Color(0x92fafafa), icon: IconData(63056, fontFamily: 'MaterialIcons'), widgetCloud: null, x: 20, y: 3, scaleBegin: 1, scaleEnd: 1.08, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 20, slideY: 0, slideDurMill: 3000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),),
-CloudWidget(cloudConfig: CloudConfig(size: 160, color: Color(0xb5fafafa), icon: IconData(63056, fontFamily: 'MaterialIcons'), widgetCloud: null, x: 140, y: 97, scaleBegin: 1, scaleEnd: 1.1, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 20, slideY: 4, slideDurMill: 2000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),),
+CloudWidget(cloudConfig: CloudConfig(size: 270, color: Color(0xcdbdbdbd), widgetCloud: null, x: 119, y: -50, scaleBegin: 1, scaleEnd: 1.1, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 11, slideY: 13, slideDurMill: 4000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),),
+CloudWidget(cloudConfig: CloudConfig(size: 250, color: Color(0x92fafafa), widgetCloud: null, x: 20, y: 3, scaleBegin: 1, scaleEnd: 1.08, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 20, slideY: 0, slideDurMill: 3000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),),
+CloudWidget(cloudConfig: CloudConfig(size: 160, color: Color(0xb5fafafa),  widgetCloud: null, x: 140, y: 97, scaleBegin: 1, scaleEnd: 1.1, scaleCurve: Cubic(0.40, 0.00, 0.20, 1.00), slideX: 20, slideY: 4, slideDurMill: 2000, slideCurve: Cubic(0.40, 0.00, 0.20, 1.00)),),
 ],
   
   )
