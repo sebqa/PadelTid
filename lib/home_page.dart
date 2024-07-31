b0e3def7aef329a3602a66861d298d9f3a193c92
sebqa <sebqa3009@gmail.com>
2024-07-31 14:20:53 +0200

    close other expanded

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,7 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/services.dart';
 import 'custom_app_bar.dart';
 import 'document_widget.dart';
+import 'main_list_view.dart';
 import 'recommended_lv_holder.dart';
 
 class HomePage extends StatefulWidget {
@@ -289,69 +290,7 @@ final Map<String, List<Document>> groupedDocuments = {};
                             'Sunday'
                           ];
 
-            return Container(
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.primary,
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(8.0,8.0,8.0,0),
-                            child: Container(
-                              decoration:  BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(16.0),
-                                  topRight: Radius.circular(16.0),
-                                ),
-                                color: Theme.of(context).colorScheme.primary,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black.withOpacity(0.25), // Shadow color with opacity
-                                    spreadRadius: 1, // Spread radius
-                                    blurRadius: 4, // Blur radius
-                                    offset: Offset(0, -1), // Offset (x, y), negative y to have the shadow on top
-                                  ),
-                                ],// Set your desired background color
-                              ),
-                              child: ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: groupedDocuments.length,
-              itemBuilder: (context, index) {
-                final date = groupedDocuments.keys.toList()[index];
-                final documentsForDate = groupedDocuments[date]!;
-                final parsedDate = DateTime.parse(date);
-
-                          documentsForDate.sort((a, b) => a.time.compareTo(b.time));
-                          
-                          final formattedDate = '${weekdayName[parsedDate
-                              .weekday - 1]}, '
-                              '${parsedDate.day}/${parsedDate
-                              .month}/${parsedDate.year}';
-                return Column(
-                  children: [
-                    ExpansionTile(
-                      shape: Border(),
-                      subtitle: Text('${documentsForDate.length} timeslots'),
-                      title: Row(
-                        children: [
-                          Icon(
-                            color: Theme.of(context).colorScheme.tertiary,
-                            Icons.calendar_today),
-                          Text(' $formattedDate'),
-                        ],
-                      ),
-                      children: [
-                        ListViewbuilder(documentsForDate: documentsForDate),
-                      ],
-                    ),
-                    Divider(thickness: 0.5,)
-                  ],
-                );
-              },
-
-            ),
-                          ),
-                        ),
-                      );
+            return MainListView(groupedDocuments: groupedDocuments, weekdayName: weekdayName);
           } else {
             return Center(
               child: Text('No data'),
