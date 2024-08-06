761b5a087e97a19aacb2b38ea155788358d5d6e6
sebqa <sebqa3009@gmail.com>
2024-08-06 09:27:47 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'home_page.dart';
 
 void main() async {
     WidgetsFlutterBinding.ensureInitialized();
-
+await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   final SharedPreferences prefs = await SharedPreferences.getInstance();
 
   runApp(MaterialApp(home:HomePage(prefs:prefs)));
