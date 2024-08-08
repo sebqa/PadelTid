bfcc57dc1d2dbade4a9dfdcc926c317ccc8de01e
sebqa <sebqa3009@gmail.com>
2024-08-08 22:16:22 +0200

    status bar farve

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -207,7 +207,14 @@ ColorScheme darkThemeColors(context) {
     title: 'PADELTID',
        theme: ThemeData.light().copyWith(
         colorScheme: darkThemeColors(context),
-        
+        appBarTheme: AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle( 
+        // Status bar color
+        statusBarColor: darkThemeColors(context).primaryContainer,
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+      ),
+        ),
         sliderTheme: SliderThemeData(
           activeTrackColor: darkThemeColors(context).tertiary,
           inactiveTrackColor: darkThemeColors(context).onPrimary,
