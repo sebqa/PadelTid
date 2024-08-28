86d99c66d4dae64e60768eb1091b405c4fc6753d
sebqa <sebqa3009@gmail.com>
2024-08-28 22:02:06 +0200

    status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 import 'home_page.dart';
 
@@ -10,9 +11,7 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 //Ask permission for notifications
-FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   messaging.onTokenRefresh.listen((fcmToken) {
     // TODO: If necessary send token to application server.
@@ -24,15 +23,21 @@ FirebaseMessaging messaging = FirebaseMessaging.instance;
   //listen for notifications
 
   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-  print('Got a message whilst in the foreground!');
-  print('Message data: ${message.data}');
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
 
-  if (message.notification != null) {
-    print('Message also contained a notification: ${message.notification?.body}');
-  }
-});
+    if (message.notification != null) {
+      print(
+          'Message also contained a notification: ${message.notification?.body}');
+    }
+  });
   runApp(MaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      appBarTheme: AppBarTheme(
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      ),
+    ),
   ));
 }
